b0f5671310de2cb73f920f310cf3b98aecad7778
Kyle Weisse <kyle.weisse@gmail.com>
2021-07-20 14:35:11 -0400

    put perimeter and area into callbakcs

--- rectangle.js
+++ rectangle.js
@@ -1,11 +1,12 @@
 module.exports = (x, y, callback) => {
-    if (x <=0 || y<=0 ) {
+    if (x <= 0 || y <= 0) {
         callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`))
     } else {
-        setTimeout()
+        setTimeout(() =>
+            callback(null, {
+                perimeter: () => 2 * (x + y),
+                area: () => x * y
+            })
+        )
     }
 }
-
-
-perimeter = (x,y) => 2 * (x+y);
-area = (x,y) =>  x * y;
